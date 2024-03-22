da5aabb5d2908683530e743face6ada4745c7488
saik434455@gmail.com <saik434455@gmail.com>
2024-03-22 15:31:17 +0530

    updating contract details to front-end scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy"
+require('dotenv').config();
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/8cAuHYtk5pZaV5S4z9QKIKLbAj3JEc3i"
 const PRIVATE_KEY = process.env.PRIVATE_KEY || '976ec26d8620bbdeff41ac071ad26407ee1bd7e03cf7b10d27067e91ccfc2ff9'
 
---
+++ scripts/update-front-end.ts
@@ -0,0 +1,44 @@
+import { deployments } from "hardhat";
+const fs = require("fs");
+const hre = require("hardhat")
+const FRONT_END_ABI_FILE = "../Ethereum_Transfer_NextJS/constants/contractABI.json";
+const FRONT_END_ADDRESS_FILE = "../Ethereum_Transfer_NextJS/constants/contractAddress.json";
+ const main = async ()=>{
+  if(process.env.UPDATE_FRONT_END){
+    console.log("Writing to front end...")
+    const contract = await deployments.get("TransferETH")
+    const contractABI = contract.abi;
+    const contractAddress = contract.address;
+    console.log("ABI updated",contractABI)
+    await updateAbi(contractABI);
+    console.log("Contract updated",contractAddress);
+    await updateContractAddress(contractAddress);
+  }
+  else{
+    console.log("NO Permission Given")
+    console.log(process.env.UPDATE_FRONT_END)
+  }
+}
+
+async function updateAbi(contractABI:any){
+  console.log("Updating the ABI to FrontEnd")
+  const abiJson = JSON.stringify(contractABI,null,2);
+  fs.writeFileSync(FRONT_END_ABI_FILE,abiJson);
+}
+
+async function updateContractAddress(contractAddress:string){
+  console.log("Updating the Address to FrontEnd");
+  try{
+    const chainId = hre.network.config.chainId;
+    if(chainId == undefined){
+      console.log("Chain ID is undefined. Cannot update contract addresses.")
+      return;
+    }
+    const addressJson = JSON.stringify({[chainId]:contractAddress},null,2);
+    fs.writeFileSync(FRONT_END_ADDRESS_FILE,addressJson)
+  }catch(error){
+    console.log("Error in updating address",error)
+  }
+}
+
+main().catch(console.error)
