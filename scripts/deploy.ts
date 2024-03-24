bf1b7360cb673f99cd3a406d36a8d280e0cf7c79
saik434455@gmail.com <saik434455@gmail.com>
2024-03-24 12:47:43 +0530

    changed abi save format

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,15 +2,19 @@ import hre from "hardhat"
 
 async function main(){
   const TransferFactory = await hre.ethers.getContractFactory("TransferETH");
-  const Transfer = await TransferFactory.deploy();
+  const transfer = await hre.ethers.getContractFactory("TransferETH");
+  const Transfer = await transfer.deploy();
 
   const contractAddress:string = typeof Transfer.target === 'string' ? Transfer.target : Transfer.target.toString();
 
   console.log(`The TransferETH Contract deployed at ${contractAddress}`)
 
-
+  const abi = TransferFactory.interface.formatJson();
+  const abiFormated = JSON.parse(abi);
+  console.log(abiFormated)
+  // work in here of sending the abi properly
   await hre.deployments.save("TransferETH",{
-    abi:TransferFactory.interface.format(),
+    abi:abiFormated,
     address:contractAddress,
   })
 }
